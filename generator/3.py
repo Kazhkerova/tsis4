d15560817c9ddbbdf016c63faac5d05e209aec54
Kazhkerova <156663451+Kazhkerova@users.noreply.github.com>
2024-02-12 16:04:50 +0600

    Delete generator directory

--- generator/3.py
+++
@@ -1,7 +0,0 @@
-def b(n):
-    a = (i for i in range(1,n+1) if i % 3 == 0 and i % 4 ==0)
-    for i in a:
-        print(i)
-
-n = int(input())
-b(n)
--- generator/5.py
+++
@@ -1,8 +0,0 @@
-def decreasing(n):
-    while n >= 0:
-        yield n
-        n -= 1
-
-n = int(input())
-for i in decreasing(n):
-    print (i)
